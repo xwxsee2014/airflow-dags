d949d512d3bc7f34b297a921ee19bc6f6de05049
Wenxing Xu <wenxingxu@nd.com.cn>
2023-03-07 22:13:35 +0800

    change crontab

--- test/sleep_and_count.py
+++ test/sleep_and_count.py
@@ -13,7 +13,7 @@ def sleep_and_count():
 # Before: define the global basic DAG
 sleep_and_count_dag = DAG(
     dag_id='sleep_and_count',
-    schedule_interval="* */10 * * *",
+    schedule_interval="* */30 * * *",
     max_active_runs=1,
     start_date=common_config.dag_start_date(),
     concurrency=3)
