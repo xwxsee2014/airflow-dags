56b706a72bcc2081f121c7e0ed3ce15c14fcda70
Wenxing Xu <wenxingxu@nd.com.cn>
2023-03-14 11:15:06 +0800

    add new task

--- test/sleep_and_count.py
+++ test/sleep_and_count.py
@@ -8,7 +8,14 @@ import time
 def sleep_and_count():
     for i in range(10):
       print(f"count update to: {i}")
-      time.sleep(60)
+      time.sleep(1)
+
+def sleep_and_sum():
+    sum = 0
+    for i in range(10):
+      sum += i
+      print(f"sum update to: {sum}")
+      time.sleep(1)
 
 # Before: define the global basic DAG
 sleep_and_count_dag = DAG(
@@ -20,5 +27,6 @@ sleep_and_count_dag = DAG(
 
 start_task = EmptyOperator(task_id='start_task', retries=3, dag=sleep_and_count_dag)
 python_task = PythonOperator(task_id='python_task', python_callable=sleep_and_count, dag=sleep_and_count_dag)
+python_task_sum = PythonOperator(task_id='python_task_sum', python_callable=sleep_and_sum, dag=sleep_and_count_dag)
 
-start_task >> python_task
+start_task >> python_task >> python_task_sum
